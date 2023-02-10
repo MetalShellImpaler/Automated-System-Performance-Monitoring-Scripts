ff3015077adbc9108c0f9380cd0cb971ec328180
A.Russo <85824539+MetalShellImpaler@users.noreply.github.com>
2023-02-10 09:11:32 +0200

    Update Automatic Performance Report.ps1

--- Automatic Performance Report.ps1
+++ Automatic Performance Report.ps1
@@ -5,16 +5,15 @@ $cpuCounter = New-Object System.Diagnostics.PerformanceCounter("Processor","% Pr
 $memCounter = New-Object System.Diagnostics.PerformanceCounter("Memory","Available MBytes")
 $diskCounter = New-Object System.Diagnostics.PerformanceCounter("LogicalDisk","% Free Space","C:")
 
-# Set the sample interval (in seconds)
 $interval = 5
 
-# Set the file path for the report
+# The file path for the report
 $filePath = "$env:USERPROFILE\Desktop\Performance_Report.txt"
 
-# Create a new file for the report
+
 New-Item -ItemType File -Path $filePath -Force
 
-# Define the report template
+# The report template
 $template = @"
 =============================================================
 Automatic Performance Report
@@ -23,27 +22,21 @@ Automatic Performance Report
 | -----------------------|:--------------:| -----------------:| -------------:|
 "@
 
-# Add the template to the report file
 Add-Content -Path $filePath -Value $template
 
 # Loop to continuously gather performance data
 while($true) {
-    # Get the current time
+    
     $time = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 
-    # Get the current CPU usage
     $cpuUsage = $cpuCounter.NextValue()
 
-    # Get the current available memory
     $memUsage = $memCounter.NextValue()
 
-    # Get the current disk usage
     $diskUsage = $diskCounter.NextValue()
 
-    # Append the performance data to the report file
     $output = "$time    $cpuUsage%       $memUsage MB      $diskUsage%"
     Add-Content -Path $filePath -Value $output
 
-    # Wait for the specified interval before gathering data again
     Start-Sleep -s $interval
 }
